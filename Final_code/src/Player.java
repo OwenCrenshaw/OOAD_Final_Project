752873ff66fcba486b0381dbe690374cc1987615
OwenCrenshaw <71041668+OwenCrenshaw@users.noreply.github.com>
2023-12-12 21:55:27 -0700

    Add files via upload

---
+++ Final_code/src/Player.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Player { // this is where each players attributes would be created. This would
+    // all show up in the scoreboard
+    private String name;
+    private int currlevel;
+    private int attempt_left;
+    private double time_completion;
+    public Player(String name){
+        this.name = name;
+    }
+
+    //observer code for adding notifying and removing observers
+
+}
